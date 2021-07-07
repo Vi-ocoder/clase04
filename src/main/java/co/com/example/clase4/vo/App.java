674813f5d6f2f417098e647e1a7dc687be0544b1
Vi-ocoder <vicotov@gmail.com>
2021-07-07 13:55:17 -0500

    Commit inicial

---
+++ src/main/java/co/com/example/clase4/vo/App.java
@@ -0,0 +1,11 @@
+package co.com.example.clase4.vo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
